ec715148b8b477833f1cccc9ef88038e2b0c1005
rmezzeluna-png <rmezzeluna@gmail.com>
2026-04-07 15:10:32 +0700

    isi obyek

--- StructInStruct.cpp
+++ StructInStruct.cpp
@@ -16,4 +16,11 @@ int main()
     //deklarasi obyek dari strucct orang
     orang mhs;
     //isi obyek
+    cout << "nama mahasiswa : " ;
+    cin >> mhs.nama;
+     cout << "alamat desa : " ;
+    cin >> mhs.alamat.desa;
+     cout << "umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
 }
