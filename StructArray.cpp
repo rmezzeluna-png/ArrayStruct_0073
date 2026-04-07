be94cf2acceeb5689fded0537f277c19e0144627
rmezzeluna-png <rmezzeluna@gmail.com>
2026-04-07 15:35:13 +0700

    structarray done

--- StructArray.cpp
+++ StructArray.cpp
@@ -10,3 +10,30 @@ struct orang{
     detailalamat alamat;
     int umur;
 };
+
+int main()
+{
+    //deklarasi obyek dari strucct orang
+    orang mhs[3];
+    //isi obyek
+    for (int i = 0; i <= 2; i++){
+    cout << "nama mahasiswa : " ;
+    getline(cin, mhs[i].nama);
+    cout << "alamat desa : " ;
+    getline(cin, mhs[i].alamat.desa);
+    cout << "umur : " ;
+    getline(cin, mhs[i].alamat.kota);
+    cout << "umur : ";
+    cin >> mhs[i].umur;
+    cout << endl;
+    }
+    cout <<endl;
+    //menampilkan isi obyek
+    for (int i = 0; i <= 2; i++)
+    {
+    cout << "nama : " << mhs[i].nama<<endl;
+    cout << "desa : " << mhs[i].alamat.desa<<endl;
+    cout << "kota : " << mhs[i].alamat.kota<<endl;
+    cout << "umur : " << mhs[i].umur<<endl;
+    }
+}
