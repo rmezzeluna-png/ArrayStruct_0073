096e5728899c23167b638d89dd0031fd70e2d820
rmezzeluna-png <rmezzeluna@gmail.com>
2026-04-07 15:08:47 +0700

    detailalamat

---
+++ StructInStruct.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+
+struct detailalamat{
+    string desa;
+    string kota;
+};
+struct orang{
+    string nama;
+    detailalamat alamat;
+    int umur;
+};
+
+int main()
+{
+    //deklarasi obyek dari strucct orang
+    orang mhs;
+    //isi obyek
+}
