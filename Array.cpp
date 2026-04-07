0412f9d00558f680d64c4484bd45535e99976d44
rmezzeluna-png <rmezzeluna@gmail.com>
2026-04-07 14:46:31 +0700

    deklarasi array dengan inisialisasi

--- Array.cpp
+++ Array.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+    //deklarasi array dengan inisialisasi
+    int dataku[s] = {10,20,30,40,50};
+    //deklarasi tanpa inisialisasi
+    int datamu[s];
+    int x;
+    
+}
